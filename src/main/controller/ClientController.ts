fc00b98a6aa2301b7efaeb479c31028c097594d1
Sebastian Pusch <seba0808@gmail.com>
2022-12-16 15:22:22 +0100

    Added error handling for clients associated to invoices

--- src/main/controller/ClientController.ts
+++ src/main/controller/ClientController.ts
@@ -6,4 +6,24 @@ export default class UserController extends Controller {
   constructor(sequelize: Sequelize) {
     super(sequelize, 'client', ['name', 'holder']);
   }
+
+  protected register(): void {
+    super.register(['delete']);
+    this.handle('delete', (id: string) => this.delete(id));
+  }
+
+  protected async delete(id: string): Promise<number> {
+    try {
+      
+      return await super.delete(id);
+    } catch (e: any) {
+
+      if (e.name === 'SequelizeForeignKeyConstraintError') {
+        
+        throw new Error('Impossibile eliminare cliente con fatture associate');
+      }
+
+      throw e;
+    }
+  }
 }
